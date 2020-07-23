6bdf6ad1e4b2498dd9570cf07ad8d03402deb100
m4dh4t <pellegrin.yann@gmail.com>
2020-07-23 17:09:57 +0200

    Cleaned up restart command (useless awaits)

--- commands/restart.js
+++ commands/restart.js
@@ -7,17 +7,17 @@ module.exports = {
         //Sending a message in DMs or in a guild is not done with the same commands
         switch (message.channel.type) {
             case ('text'): //The message was sent in a guild
-                await message.client.cache.set('isTextChannel', true);
-                await message.client.cache.set('restartNoticeChannelId', message.channel.id); //Store the channel id where the bot was restarted
+                message.client.cache.set('isTextChannel', true);
+                message.client.cache.set('restartNoticeChannelId', message.channel.id); //Store the channel id where the bot was restarted
                 break;
             case ('dm'): //The message was sent in DMs
-                await message.client.cache.set('isTextChannel', false);
-                await message.client.cache.set('restartNoticeAuthorId', message.author.id); //Store the id of the author that restarted the bot
+                message.client.cache.set('isTextChannel', false);
+                message.client.cache.set('restartNoticeAuthorId', message.author.id); //Store the id of the author that restarted the bot
                 break;
             default:
-                await console.log('Unexpected channel type');
+                console.log('Unexpected channel type');
         }
-        await message.client.cache.set('hasRestarted', true);
+        message.client.cache.set('hasRestarted', true);
         process.exit(); //Kills the process, but the bot is automatically restarted by pm2
 	},
 };
