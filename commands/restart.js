0ebe65958416cf5b386107f92985cb74f4064d2b
m4dh4t <pellegrin.yann@gmail.com>
2020-08-14 23:31:13 +0200

    Add white space at the start of the comments for readibility

--- commands/restart.js
+++ commands/restart.js
@@ -4,20 +4,20 @@ module.exports = {
     adminOnly: true,
 	async execute(message, args) {
         await message.reply('Restarting...');
-        //Sending a message in DMs or in a guild is not done with the same commands
+        // Sending a message in DMs or in a guild is not done with the same commands
         switch (message.channel.type) {
-            case ('text'): //The message was sent in a guild
+            case ('text'): // The message was sent in a guild
                 message.client.restart.set('channelType', 'guild');
-                message.client.restart.set('noticeChannelId', message.channel.id); //Store the channel id where the bot was restarted
+                message.client.restart.set('noticeChannelId', message.channel.id); // Stores the channel id where the bot was restarted
                 break;
-            case ('dm'): //The message was sent in DMs
+            case ('dm'): // The message was sent in DMs
                 message.client.restart.set('channelType', 'dm');
-                message.client.restart.set('noticeAuthorId', message.author.id); //Store the id of the author that restarted the bot
+                message.client.restart.set('noticeAuthorId', message.author.id); // Stores the id of the author that restarted the bot
                 break;
             default:
                 console.log('Restart command issued in an unexpected channel type');
         }
         message.client.restart.set('restarted');
-        process.exit(); //Kills the process, but the bot is automatically restarted by pm2
+        process.exit(); // Kills the process, but the bot is automatically restarted by pm2
 	},
 };
