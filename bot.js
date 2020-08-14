ccfd61f69c7d3319024d65114409e20744184eb6
m4dh4t <pellegrin.yann@gmail.com>
2020-08-14 23:36:48 +0200

    Rewrite several bits of code
    - Order beginning code and add comments
    - Change admins enmap table to botusers for future updates
    - Initialize the bot admins array with the bot owner from config.json
    - Change prefix organization, separate the main prefix from the ones to override
    - Remove the command cooldown in a private chat with the bot
    - If an error is thrown with the message "InvalidArgument", the bot now tells it to the user

--- bot.js
+++ bot.js
@@ -1,14 +1,19 @@
-const fs = require('fs');
+// Adds required modules
 global.Discord = require('discord.js');
+const { mainPrefix, overridenPrefixes, token, ownerId } = require('./config.json');
+const Enmap = require('enmap');
+const fs = require('fs');
+
+// Creates required variables
 const client = new Discord.Client(); 
-const { prefixes, token } = require('./config.json');
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 const cooldowns = new Discord.Collection();
-const Enmap = require('enmap');
-client.restart = new Enmap({name: 'restart'});
-client.admins = new Enmap({name: 'admins'});
 
-// Build the command collection based on the files in the ./commands folder
+// Creates required enmap databases
+client.restart = new Enmap({name: 'restart'});
+client.botusers = new Enmap({name: 'botusers'});
+
+// Builds the command collection based on the files in the ./commands folder
 client.commands = new Discord.Collection();
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
@@ -18,10 +23,10 @@ for (const file of commandFiles) {
 client.once('ready', () => {
     console.log('Ready!');
 
-    // Check for the enmap database to correctly initialize
+    // Checks for the enmap database to correctly initialize
     client.restart.defer.then( async () => {
-        console.log(client.restart.size + ' keys loaded');
-        // If the bot has been restarted, send a notice at the right place when ready
+        console.log(client.restart.size + ' keys loaded in restart enmap');
+        // If the bot has been restarted, sends a notice at the right place when ready
         if (client.restart.has('restarted')) {
             switch (client.restart.get('channelType')) {
                 case ('guild'):
@@ -38,56 +43,67 @@ client.once('ready', () => {
         }
         client.restart.clear();
     });
+
+    // Checks for the enmap database to correctly initialize
+    client.botusers.defer.then( () => {
+        console.log(client.botusers.size + ' keys loaded in botusers enmap');
+        // Ensures that the bot admins array load at least the bot owner set in config.json
+        client.botusers.ensure("admins", [ownerId]);
+    });
 });
 
 client.on('message', message => {
     console.log(message.content);
 
-    // We need to check for multiple possible prefixes, as the bot needs to be able modify answer from other bots
+    // Checks for multiple possible prefixes, as the bot needs to be able modify answer from other bots
     var prefix;
     if (!message.author.bot) {
-        for (let i = 0; i < prefixes.length; ++i) {
-            if (message.content.startsWith(prefixes[i])) {
-                prefix = prefixes[i];
-                break;
-            } 
+        if (!message.content.startsWith(mainPrefix)) {
+            for (let i = 0; i < overridenPrefixes.length; ++i) {
+                if (message.content.startsWith(overridenPrefixes[i])) {
+                    prefix = overridenPrefixes[i];
+                    break;
+                } 
+            }
+            if (!prefix) return;
+        } else {
+            prefix = mainPrefix;
         }
-        if (prefix == null) return;
     } else return;
 
-    // Separate the command name and arguments from the prefix
+    // Separates the command name and arguments from the prefix
     const args = message.content.slice(prefix.length).trim().split(/ +/);
     const commandName = args.shift().toLowerCase();
 
-    // Check if the given command command exists in the command collection or is an alias, else exit
+    // Checks if the given command command exists in the command collection or is an alias, else exit
     const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
     if (!command) return;
 
-    // Check if the given prefix is the default one, and if not, check if the given command is supposed to override another bot
-    if (prefix !== prefixes[0] && prefix !== command.overridingBotPrefix) return;
+    // Checks if the given prefix is the default one, and if not, check if the given command is supposed to override another bot
+    if (prefix !== mainPrefix && prefix !== command.overridenPrefix) return;
 
-    // Check if the user has the rights to execute the command
-    if (command.adminOnly && !client.admins.get('adminsList').includes(message.author.id)) {
+    // Checks if the user has the rights to execute the command
+    if (command.adminOnly && !client.botusers.includes('admins', message.author.id)) {
         return message.channel.send('You must be a bot administrator to use this command !');
     }
 
-    // Check if the command is valid for DMs usage
+    // Checks if the command is valid for DMs usage
     if (command.guildOnly && message.channel.type !== 'text') {
         return message.reply('I cannot execute that command inside DMs.');
     }
 
-    // Check if the command args are valid
+    // Checks if the command args are valid
     if (command.args && !args.length) {
         let reply = `You did not provide any arguments, ${message.author}.`;
 
 		if (command.usage) {
-            reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+            reply += `\nUsage: \`${prefix}${command.name} ${command.usage}\``;
 		}
 
 		return message.channel.send(reply);
     }
 
-    // Setup the cooldown and verification before execution
+    // Setups the cooldown and verification before execution
     if (!cooldowns.has(command.name)) {
         cooldowns.set(command.name, new Discord.Collection());
     }
@@ -97,7 +113,7 @@ client.on('message', message => {
     if (timestamps.has(message.author.id)) { // Timestamp verification linked to the author id to allow other to still use the command
         const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
     
-        if (now < expirationTime) {
+        if (now < expirationTime && message.channel.type === 'text') { // Allows a user to spam his own chat with the bot if he wants to
             const timeLeft = (expirationTime - now) / 1000;
             return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
         }
@@ -105,12 +121,16 @@ client.on('message', message => {
     timestamps.set(message.author.id, now);
     setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
 
-    //Command execution and bug handler
+    // Command execution and bug handler
     try {
         command.execute(message, args);
     } catch (error) {
-        console.error(error);
-        message.reply('there was an error trying to execute that command.');
+        if (error.message === 'InvalidArgument') {
+            message.channel.send(`You did not provide any valid argument ${message.author}.\nUsage: \`${prefix}${command.name} ${command.usage}\``);
+        } else {
+            console.error(error);
+            message.channel.send('There was an error trying to execute that command.');
+        }
     }
 });
 
