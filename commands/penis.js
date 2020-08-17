ea20b75e5fa1bda98884ae981893da7454443a55
siphore <michael@cheneval.com>
2020-08-17 03:04:20 +0200

    updates penis.js command

--- commands/penis.js
+++ commands/penis.js
@@ -1,111 +1,90 @@
 module.exports = {
-    name: 'penis',
+	name: 'penis',
     description: 'Gives your penis size (100% accurate).',
-	usage: '[@Username]',
-    cooldown: 3,
-    aliases: ['pp', 'pepou', 'dingdong'],
-    overridenPrefix: 'pls',
+    usage: '[@Username]',
+	cooldown: 3,
+	aliases: ['pp', 'pepou', 'dingdong'],
 	execute(message, args) {
-        const { mainPrefix, overridenPrefixes } = require('../config.json');
-        
-        const taggedUser = message.mentions.users.first();
-        const delay = 100;
-        
-        let random = Math.floor(Math.random() * 11); //Nombre de "=" -> entre 0 et [nombre-1]
-        let txt = '';
-        let msgToSend = '';
-        let argsTxt = '';
-        let replace = false;
-        let a = 0;
-        
-        if (message.content.startsWith(mainPrefix)) { //*...
-            if (!message.mentions.users.size) { //*penis
-                if (!args.length) {
-                    txt = `${message.author.username}'s penis\n8`;
-                
-                    switch (message.author.id) {
-                        case '219175344199041035': //Micha
-                            random = 30;
-                            break;
+		const { overridenPrefixes } = require('../config.json');
 
-                        case '321006216887402496': //Kento
-                            random = 0;
-                            break;
-                    }
-                } else { //*penis + args
-                    let arguments = args.toString().toLowerCase();
-                    switch (arguments) {
-                        case "noich":
-                            random = 0;
-                            break;
-                    }
+		const taggedUser = message.mentions.users.first();
+		let random = Math.floor(Math.random() * 11); //Nombre de "=" -> entre 0 et [nombre-1]
+		let txt = '';
+		let argsTxt = '';
+		let replace = false;
+		let a = 0;
 
-                    for (let i = 0 ; i < args.length ; i++) {
-                        argsTxt += args[i];
-                        if (i < (args.length-1)) {
-                            argsTxt += ' ';
-                        }
-                    }
-                    txt = `${argsTxt}'s penis\n8`;
-                }
-            } else { //*penis @...
-                txt = `${taggedUser.username}'s penis\n8`;
-                
-                switch (taggedUser.id) {
-                    case '219175344199041035': //Micha
-                        random = 30;
-                        break;
+		if (!message.mentions.users.size) { //*penis
+			if (!args.length) {
+				txt = `${message.author.username}'s penis\n8`;
 
-                    case '321006216887402496': //Kento
-                        random = 0;
-                        break;
-                }
-            }
-            
-            //txt writing
-            while (a < random) {
-                txt += '=';
-                a++;
-            }
-            txt += 'D';
-            
-            const penisEmbed = new Discord.MessageEmbed()
-                .setColor('#'+(Math.random()*0xFFFFFF<<0).toString(16))
-                .setTitle('real peepee size machine')
-                .setDescription(txt);
-            
-            message.channel.send(penisEmbed);
-        } else if (message.content.startsWith('pls')) { // Overrides Dank Memer bot command
-            if (!message.mentions.users.size) { //pls penis
-                if (message.author.id === '321006216887402496') { //Kento
-                    setTimeout(function() {
-                        message.channel.bulkDelete(1, true);
-                        
-                        msgToSend = `${message.author.username}'s penis\n8D`;
-                        replace = true;
-                    }, delay);
-                }
-            } else { //pls penis @...
-                if (taggedUser.id === '321006216887402496') { //Kento
-                    setTimeout(function() {
-                        message.channel.bulkDelete(1, true);
-                        
-                        msgToSend = `${taggedUser.username}'s penis\n8D`;
-                        replace = true;
-                    }, delay);
-                }
-            }
-            
-            setTimeout(function() {
-                if (replace) {
-                    const plsPenisEmbed = new Discord.MessageEmbed()
-                        .setColor('#'+(Math.random()*0xFFFFFF<<0).toString(16))
-                        .setTitle('real peepee size machine')
-                        .setDescription(msgToSend);
+				switch (message.author.id) {
+				case '219175344199041035': //Micha
+					random = 30;
+					break;
 
-                    message.channel.send(plsPenisEmbed);
-                }
-            }, delay + 50);
-        }
+				case '321006216887402496': //Kento
+					random = 0;
+					replace = true;
+					break;
+				}
+			}
+			else { //*penis + args
+				if (!message.content.startsWith(overridenPrefixes)) {
+					const arguments = args.toString().toLowerCase();
+					switch (arguments) {
+					case 'noich':
+						random = 0;
+						break;
+					}
+
+					for (let i = 0 ; i < args.length ; i++) {
+						argsTxt += args[i];
+						if (i < (args.length - 1)) {
+							argsTxt += ' ';
+						}
+					}
+					txt = `${argsTxt}'s penis\n8`;
+				}
+			}
+		}
+		else { //*penis @...
+			txt = `${taggedUser.username}'s penis\n8`;
+
+			switch (taggedUser.id) {
+			case '219175344199041035': //Micha
+				random = 30;
+				replace = true;
+				break;
+
+			case '321006216887402496': //Kento
+				random = 0;
+				replace = true;
+				break;
+			}
+		}
+
+		if (message.content.startsWith(overridenPrefixes) && replace) {
+			message.channel.awaitMessages(m => m.author.id == '270904126974590976', //bot deletes dank memer's message
+				{ max: 1, time: 3000 }).then(collected => {
+				collected.first().delete();
+			});
+		}
+
+		if (!message.content.startsWith(overridenPrefixes) && message.author.id != '321006216887402496' || replace) {
+			//txt writing
+			while (a < random) {
+				txt += '=';
+				a++;
+			}
+			txt += 'D';
+
+			const penisEmbed = new Discord.MessageEmbed()
+				.setColor('#' + (Math.random() * 0xFFFFFF << 0).toString(16))
+				.setTitle('real peepee size machine')
+				.setDescription(txt);
+
+			message.channel.send(penisEmbed);
+		}
 	},
 };
