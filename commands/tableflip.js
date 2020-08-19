06db180b4e2eda1497c584e2ecad6d287e836d21
Pace222 <pierugo.pace@epfl.ch>
2020-08-19 12:37:59 +0200

    Implemented tableflip.js

---
+++ commands/tableflip.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'tableflip',
+    description: 'Sends a tableflip',
+    cooldown: 0.5,
+    args: false,
+    execute(message, args) {
+        message.channel.send('(╯°□°）╯︵ ┻━┻');
+    },
+}
