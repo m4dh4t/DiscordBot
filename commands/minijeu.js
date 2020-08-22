a1fd52e68e7ab409a15461cc563277993e58552d
siphore <michael@cheneval.com>
2020-08-22 16:42:03 +0200

    Updates minijeu.js to add multiple tries in the anagram minigame and to display a clearer result embed

--- commands/minijeu.js
+++ commands/minijeu.js
@@ -5,59 +5,68 @@ module.exports = {
     //cooldown: 180,
 	execute(message, args) {
         const randomWords = require('random-words');
-        const timeToResolve = 30;
+        const timeToResolve = 5;
         var correctWord = randomWords();
-        var tCorrectWord = correctWord.split('');
         var mixedWord = '';
         var compteur = 0;
-        var answerFound = false;
+        var t0;
+        //******************************************************************************************************************************************************************************************************* Règles
 
         const gameEmbed = new Discord.MessageEmbed()
             .setColor('#'+(Math.random()*0xFFFFFF<<0).toString(16))
             .setTitle('ÉPREUVE !')
             .setDescription(message.author.username + ', tu as **' + timeToResolve + ' secondes** pour réussir ce challenge. Pour ce faire, tu dois réarranger les lettres ci-dessous pour former un mot ANGLAIS existant.\nBonne chance !');
 
-        const answerEmbed = new Discord.MessageEmbed()
-            .setColor('#'+(Math.random()*0xFFFFFF<<0).toString(16))
-            .setAuthor(message.author.username)
-            .setTitle('Résultat:');
-        //******************************************************************************************************************************************************************************************************* Règles
-
-        while (tCorrectWord.length) {
-            const random = Math.floor(Math.random() * tCorrectWord.length);
-            mixedWord += tCorrectWord[random];
-            tCorrectWord.splice(random, 1);
-            compteur++;
-        }
+        do {
+            var tCorrectWord = correctWord.split('');
+            while (tCorrectWord.length) {
+                const random = Math.floor(Math.random() * tCorrectWord.length);
+                mixedWord += tCorrectWord[random];
+                tCorrectWord.splice(random, 1);
+                compteur++;
+            }
+        } while (mixedWord === correctWord);
         gameEmbed.addField('Lettres:', mixedWord);
 
         message.channel.send(gameEmbed);
+        startChrono();
+        awaitMessage();
         //******************************************************************************************************************************************************************************************************* Réponse
 
-        message.channel.awaitMessages(m => m.author.id == message.author.id, // waits for player to put an answer
-            { max: 1, time: (timeToResolve * 1000) }).then(collected => {
-                var answer = collected.first().toString();
+        const answerEmbed = new Discord.MessageEmbed()
+            .setColor('#'+(Math.random()*0xFFFFFF<<0).toString(16))
+            .setAuthor(message.author.username, message.author.displayAvatarURL({ format: "png", dynamic: true }))
+            .setTitle('RÉSULTAT:')
 
-                if (answer === correctWord) {
-                    //answerEmbed.title = 'Bravo !';
-                    answerEmbed.addField('Bravo !', 'Le mot était bien **' + correctWord + '** !');
-                    answerFound = true;
-                } else {
-                    //answerEmbed.title = 'Raté !';
+        function startChrono () {
+            var date = new Date();
+            t0 = date.getTime();
+        }
+
+        function readChrono () {
+            var date = new Date();
+            var t = date.getTime();
+            var dT = Math.floor((t-t0));
+            return dT;
+        }
+
+        function awaitMessage () {
+            message.channel.awaitMessages(m => m.author.id == message.author.id, // waits for player to put an answer
+                { max: 1, time: ((timeToResolve * 1000) - readChrono()) }).then(collected => {
+                    var answer = collected.first().toString();
+                    testAnswer(answer);
+                }).catch(() => {
                     answerEmbed.addField('Raté !', 'Le mot à trouver était **' + correctWord + '** :(');
-                    //answerEmbed.description = 'Le mot à trouver était "' + correctWord + '" :(';
-                }
-    
-                message.channel.send(answerEmbed);
-            }).catch(() => {
-                timeout();
-            });
+                    message.channel.send(answerEmbed);
+                });
+        }
 
-        function timeout () {
-            if (!answerFound) {
-                answerEmbed.title = 'RATÉ !';
-                answerEmbed.description = 'Tu as pris trop de temps pour donner ta réponse !\nLe mot à trouver était **' + correctWord + '** :(';
+        function testAnswer (answer) {
+            if (answer === correctWord) {
+                answerEmbed.addField('Bravo !', 'Le mot était bien **' + correctWord + '** !');
                 message.channel.send(answerEmbed);
+            } else {
+                awaitMessage();
             }
         }
 	},
