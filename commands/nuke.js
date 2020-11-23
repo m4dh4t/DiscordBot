1ff885d8ed09e4194f8d00e530b59932add7e187
siphore <michael@cheneval.com>
2020-11-23 23:50:45 +0100

    Updates the channel id for the new bots-sandbox channel (the first one was too old and couldn't be completely nuked)

--- commands/nuke.js
+++ commands/nuke.js
@@ -4,14 +4,14 @@ module.exports = {
     guildOnly: true,
     aliases: ['💣'],
 	async execute(message, args) {
-        if(message.channel.id == '730207114399580161'){
+        if (message.channel.id == '780565449913925642') {
             var nbMessages = 100;
             while (nbMessages === 100) {
                 await message.channel.bulkDelete(100).then(messages => nbMessages = messages.size);
             }
             message.channel.send(`<@${message.author.id}>\n> ${message.content}`, { files: ['http://www.quickmeme.com/img/cf/cfe8938e72eb94d41bbbe99acad77a50cb08a95e164c2b7163d50877e0f86441.jpg'] });
         } else {
-            message.channel.send('This command is not authorized outside of the bots sandbox !');
+            message.channel.send('This command is not authorized outside of the bots-sandbox channel !');
         }
 	},
 };
