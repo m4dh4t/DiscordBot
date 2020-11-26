7c0d9ea38967f242a079ccf9db665f1c6fb670c6
siphore <michael@cheneval.com>
2020-11-26 21:35:44 +0100

    Adds time at the end (still has LOTS of bugs with French alternative)

--- commands/minijeu.js
+++ commands/minijeu.js
@@ -2,14 +2,47 @@ module.exports = {
 	name: 'minigame',
 	description: 'minigame to get free of mute',
     aliases: ['game', 'minijeu'],
-    //cooldown: 180,
+    // cooldown: 30,
 	execute(message, args) {
-        const randomWords = require('random-words');
+        // Global
+        const axios = require('axios');
+        const cheerio = require('cheerio');
         const timeToResolve = 30;
-        var correctWord = randomWords();
-        var mixedWord = '';
-        var compteur = 0;
+        // var correctWord;
+        const correctWord = { content:'' };
         var t0;
+        //var remainingTime;
+
+        // French
+        // function getRandomWord(language, obj) {
+        //     axios.get(language)
+        //         .then( response => {
+        //             if(response.status === 200) {
+        //                 const html = response.data;
+        //                 const $ = cheerio.load(html);
+        //                 obj.content = $('#firstHeading').text();
+        //             }
+        //         })
+        //         .catch( error => {
+        //             console.log(error);
+        //         });
+        // }
+
+        function getRandomWord(language, obj) {
+            axios.get(language)
+                .then(function(response) {
+                    if(response.status === 200) {
+                        const html = response.data;
+                        const $ = cheerio.load(html);
+                        obj.content = ($('#firstHeading').text());
+                        // message.channel.send($('#firstHeading').text().toString());
+                    }
+                })
+                .catch(function(error) {
+                    console.log(error);
+                });
+        }
+        const french = 'https://fr.wiktionary.org/wiki/Sp%C3%A9cial:Page_au_hasard_dans_une_cat%C3%A9gorie/Noms_communs_en_fran%C3%A7ais';
         //******************************************************************************************************************************************************************************************************* Règles
 
         const gameEmbed = new Discord.MessageEmbed()
@@ -17,15 +50,35 @@ module.exports = {
             .setTitle('ÉPREUVE !')
             .setDescription(message.author.username + ', tu as **' + timeToResolve + ' secondes** pour réussir ce challenge. Pour ce faire, tu dois réarranger les lettres ci-dessous pour former un mot ANGLAIS existant.\nBonne chance !');
 
+        // switch (args[0]) {
+        //     case ('fr'):
+        //         getRandomWord(french, correctWord);
+        //         // correctWord.content = 'ok';
+        //         break;
+
+        //     case ('eng'):
+        //         const randomWords = require('random-words');
+        //         correctWord.content = randomWords();
+        //         break;
+
+        //     default:
+        //         throw new Error('InvalidArgument');
+        // }
+
+        const randomWords = require('random-words');
+        correctWord.content = randomWords();
+
         do {
-            var tCorrectWord = correctWord.split('');
+            const tCorrectWord = correctWord.content.split('');
+            var mixedWord = '';
+            var compteur = 0;
             while (tCorrectWord.length) {
                 const random = Math.floor(Math.random() * tCorrectWord.length);
                 mixedWord += tCorrectWord[random];
                 tCorrectWord.splice(random, 1);
                 compteur++;
             }
-        } while (mixedWord === correctWord);
+        } while (mixedWord === correctWord.content);
         gameEmbed.addField('Lettres:', mixedWord);
 
         message.channel.send(gameEmbed);
@@ -35,8 +88,8 @@ module.exports = {
 
         const answerEmbed = new Discord.MessageEmbed()
             .setColor('#'+(Math.random()*0xFFFFFF<<0).toString(16))
-            .setAuthor(message.author.username, message.author.displayAvatarURL({ format: "png", dynamic: true }))
-            .setTitle('RÉSULTAT:')
+            .setAuthor(message.author.username, message.author.displayAvatarURL({ format: 'png', dynamic: true }))
+            .setTitle('RÉSULTAT:');
 
         function startChrono () {
             var date = new Date();
@@ -47,23 +100,28 @@ module.exports = {
             var date = new Date();
             var t = date.getTime();
             var dT = Math.floor((t-t0));
-            return dT;
+            const remainingTime = ((timeToResolve * 1000) - dT);
+            return remainingTime;
         }
 
         function awaitMessage () {
             message.channel.awaitMessages(m => m.author.id == message.author.id, // waits for player to put an answer
-                { max: 1, time: ((timeToResolve * 1000) - readChrono()) }).then(collected => {
-                    var answer = collected.first().toString();
+                { max: 1, time: readChrono() }).then(collected => {
+                    var answer = collected.first().toString().toLowerCase();
                     testAnswer(answer);
                 }).catch(() => {
-                    answerEmbed.addField('Raté !', 'Le mot à trouver était **' + correctWord + '** :(');
+                    answerEmbed.addField('Raté !', 'Le mot à trouver était **' + correctWord.content + '** :(');
                     message.channel.send(answerEmbed);
                 });
         }
 
         function testAnswer (answer) {
-            if (answer === correctWord) {
-                answerEmbed.addField('Bravo !', 'Le mot était bien **' + correctWord + '** !');
+            if (answer.startsWith('testm game')) return;
+
+            if (answer === correctWord.content) {
+                const remainingTimeInSeconds = (readChrono() / 1000).toFixed(1);
+                answerEmbed.addField('Bravo !', 'Le mot était bien **' + correctWord.content + '** !');
+                answerEmbed.setFooter('Temps restant: ' + remainingTimeInSeconds + 's');
                 message.channel.send(answerEmbed);
             } else {
                 awaitMessage();
