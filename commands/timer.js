b4b771fdb0c3fd76c1ef2061257179f1a7ca4628
siphore <michael@cheneval.com>
2021-05-21 03:12:13 +0200

    Few changes to timer.js to make it more convenient to use

--- commands/timer.js
+++ commands/timer.js
@@ -3,34 +3,54 @@ module.exports = {
 	description: 'sets a timer',
     cooldown: 5,
 	args: true,
-	usage: '[h min s]',
+	usage: '[hh mm ss | mm ss | ss]',
 	execute(message, args) {
-        let structure = /\d\d \d\d \d\d/;
+        let structure1 = /\d\d \d\d \d\d/;
+		let structure2 = /\d\d \d\d/;
+		let structure3 = /\d\d/;
 
-		if (!message.content.match(structure)) {
+		if (!message.content.match(structure1) && !message.content.match(structure2) && !message.content.match(structure3)) {
 			throw new Error('InvalidArgument');
 		}
 
+		let hours   = 0;
+		let minutes = 0;
+		let seconds = 0
         const time = args.toString().split(',');
-        hours   = parseInt(time[0]);
-        minutes = parseInt(time[1]);
-        seconds = parseInt(time[2]);
+
+		switch(time.length)  {
+			case 3:
+				hours   = parseInt(time[0]);
+				minutes = parseInt(time[1]);
+				seconds = parseInt(time[2]);
+				break;
+
+			case 2:
+				minutes = parseInt(time[0]);	
+				seconds = parseInt(time[1]);
+				break;
+
+			case 1:
+				seconds = parseInt(time[0]);
+				break;
+		}
+
 		message.channel.send(`\`\`\`fix\nTimer set for${hours != 0 ? ' ' + hours + 'h' : ''}${minutes != 0 ? ' ' + minutes + 'min' : ''}${seconds != 0 ? ' ' + seconds + 's' : ''}\n\`\`\``);
 
-		var chrono;
+		let chrono;
 		let totalTime = hours*3600 + minutes*60 + seconds;
 
 		(function() {
 			clearInterval(chrono)
-			var laDate = new Date();
+			let laDate = new Date();
 			t0 = laDate.getTime();
 			chrono = setInterval(lectureChrono, 10);
 		}());
 
 		function lectureChrono () {
-			var laDate = new Date();
-			var t = laDate.getTime();
-			var dt = Math.floor((t-t0)/1000);
+			let laDate = new Date();
+			let t = laDate.getTime();
+			let dt = Math.floor((t-t0) / 1000);
 
 			if (dt >= totalTime) {
 				message.reply("TIMER IS OVER !");
